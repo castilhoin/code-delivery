64953490996a0cbd1502c5df1736b9d9d5de6840
Pedro Castilho <pedro@castilho.in>
2020-09-03 09:50:24 -0300

    Add cards, logo and about page

--- code_delivery/ext/site/main.py
+++ code_delivery/ext/site/main.py
@@ -6,3 +6,7 @@ bp = Blueprint("site", __name__)
 @bp.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@bp.route("/about")
+def about():
+    return render_template("about_us.html")
